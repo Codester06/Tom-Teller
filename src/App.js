ee40bbd08f0fab141a4e6cec8f2cd1132ae52281
Vivek Sharma <viveksharma.vs0615@gmail.com>
2026-01-10 21:32:25 +0530

    joinus

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import CountdownSignup from './components/CountdownSignup';
 import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      {/* Background will be handled via CSS */}
-      <CountdownSignup />
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          {/* Redirect root path to /joinus */}
+          <Route path="/" element={<Navigate to="/joinus" replace />} />
+          
+          {/* Main signup page */}
+          <Route path="/joinus" element={<CountdownSignup />} />
+          
+          {/* Catch all other routes and redirect to /joinus */}
+          <Route path="*" element={<Navigate to="/joinus" replace />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
